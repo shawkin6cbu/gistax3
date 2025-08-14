53c61cb91a34b4c23474b6969e3e4a82fae12a57
shawkin6cbu <34901592+shawkin6cbu@users.noreply.github.com>
2025-08-14 13:38:46 -0500

    Add files via upload
    GUI updates

---
+++ gistax3/desoto/services/__init__.py
@@ -0,0 +1,3 @@
+from .parcels import query as query_parcels
+from .tax import fetch_total, DISTRICT_OPTIONS
+from .tax_document import process_tax_document, extract_tax_info_from_pdf, parse_tax_text
