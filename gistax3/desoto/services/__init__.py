b9c558181e30836a07a71e02c057d80af70036fa
shawkin6cbu <34901592+shawkin6cbu@users.noreply.github.com>
2025-08-22 10:26:13 -0500

    Delete gistax3 directory

--- gistax3/desoto/services/__init__.py
+++
@@ -1,4 +0,0 @@
-from .parcels import query as query_parcels
-from .tax import fetch_total, DISTRICT_OPTIONS
-from .tax_document import process_tax_document, extract_tax_info_from_pdf, parse_tax_text
-from .document_splitter import process_comprehensive_document     
--- gistax3/main.py
+++
@@ -1,9 +0,0 @@
-from desoto.app import App
-
-if __name__ == "__main__":
-    try:
-        import ctypes
-        ctypes.windll.shcore.SetProcessDpiAwareness(1)
-    except Exception:
-        pass
-    App().mainloop()
